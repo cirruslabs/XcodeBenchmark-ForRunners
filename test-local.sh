358ae3aff392036a801b97510339412e43fb111a
Fedor Korotkov <fedor.korotkov@gmail.com>
2025-08-20 15:04:56 -0400

    4 jobs

--- test-local.sh
+++ test-local.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /tmp/fiotest
 
-/opt/homebrew/bin/fio --name=write_throughput --directory=/tmp/fiotest --numjobs=8 --size=10G --time_based --runtime=60s --ramp_time=2s --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1
+/opt/homebrew/bin/fio --name=write_throughput --directory=/tmp/fiotest --numjobs=4 --size=10G --time_based --runtime=60s --ramp_time=2s --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1
 
 rm -rf /tmp/fiotest
